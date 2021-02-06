bad50af6bc8beaa6762952c55c5faac9aae47af5
mcraig567 <craig.matthewt@gmail.com>
2021-02-06 14:41:56 -0500

    Create toast for expired spells

--- turn_end.js
+++ turn_end.js
@@ -13,6 +13,10 @@ document.addEventListener("DOMContentLoaded", function() {
 function endTurn() {
     console.log('Next Turn')
 
+    if (document.querySelector('#end_turn').innerHTML === "Start Battle") {
+        document.querySelector('#end_turn').innerHTML = "End Turn";
+    };
+
     //Get list of all players
     let players = document.querySelectorAll('.player_list');
     players = Array.from(players);
@@ -24,13 +28,10 @@ function endTurn() {
     if (turn === players.length - 1) {
         turn = 0;
 
+        //All players have gone (in 6 seconds), so reduce all spells by 6 seconds
         //Get list of all remaining spell durations (li elements)
         let spell_times = document.querySelectorAll('.time');
         //console.log(spell_times)
-        
-        if (document.querySelector('#end_turn').innerHTML === "Start Battle") {
-            document.querySelector('#end_turn').innerHTML = "End Turn";
-        };
 
         let i;
         //Iterate through spells, decreasing time by 6 seconds, and removing if time is below 0
@@ -39,12 +40,31 @@ function endTurn() {
             time -= 6;
 
             if (time <= 0) {
+                //Create text to be added to expired spell toast and add to toast div
+                let spellName = spell_times[i].parentNode.parentNode.childNodes[0].innerHTML;
+                console.log(`Removing Spell ${spellName}`);
+
+                let caster = spell_times[i].parentNode.childNodes[1].dataset.value;
+                console.log(`Caster ${caster}`);
+
+                let new_div = document.createElement('div');
+                new_div.innerHTML = `${spellName} (${caster}) has expired`;
+
+                document.querySelector('#toast').appendChild(new_div);
+
+                //Remove spell from page
                 spell_times[i].parentNode.parentNode.parentNode.removeChild(spell_times[i].parentNode.parentNode);
+
             } else {
                 spell_times[i].value = time;
                 spell_times[i].innerHTML = `Remaining Time: ${spell_times[i].value}s`;
             };        
         };
+
+        //Only show toast if spells have expired
+        if (document.querySelector('#toast').innerHTML != "") {
+            showToast();
+        }
 
     } else {
         turn ++
@@ -71,3 +91,13 @@ function endBattle() {
     turn = -1;
     document.querySelector('#end_turn').innerHTML = "Start Battle";
 };
+
+function showToast() {
+    console.log("Showing Toast")
+    let x = document.querySelector('#toast');
+    x.className = "show";
+    setTimeout(function() {
+        x.className = x.className.replace("show", "");
+        x.innerHTML = "";
+    }, 3000);
+}
